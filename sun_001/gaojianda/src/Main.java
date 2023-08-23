2e333281b2bf35918e5923f6ba2a3771ee33e790
lv123123long <a1749907938@outlook.com>
2023-08-24 00:09:21 +0800

    20230824

--- sun_001/gaojianda/src/Main.java
+++ sun_001/gaojianda/src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public static void main(String[] args) {
 
@@ -6,11 +5,11 @@ public class Main {
 
 //        Person person = new Person();   // 在使用类创建对象的时候，，构造器就会被调用
 //        System.out.print(person.name);
-        StaticPerson staticPerson = new StaticPerson();
-        StaticPerson staticPerson1 = new StaticPerson();
-        StaticPerson.info = "Test";
-
-        System.out.println(StaticPerson.info);
-        StaticPerson.test(); 
+//        StaticPerson staticPerson = new StaticPerson();
+//        StaticPerson staticPerson1 = new StaticPerson();
+//        StaticPerson.info = "Test";
+//
+//        System.out.println(StaticPerson.info);
+//        StaticPerson.test();
     }
 }
---
+++ sun_001/gaojianda/src/com/test/HeHao.java
@@ -0,0 +1,28 @@
+package com.test;
+
+/**
+ * @author: Lvlongxin
+ * @description 封装
+ * @date: 2023/8/23 23:30
+ */
+
+public class HeHao {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
